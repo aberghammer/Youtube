d2abe80c485c66d4e3e137e4b9b0486663848bdc
Andreas <andreas.berghammer@gmx.de>
2023-11-13 16:19:16 +0100

    initial commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import { Web3Connect } from '@/components/Web3Connect'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className='flex flex-col justify-center items-center'><Web3Connect /></div>
+    </main>
+  )
+}
---
+++ frontend/config/wagmi.tsx
@@ -0,0 +1,50 @@
+"use client"
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+    getDefaultWallets,
+    RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import {
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    zora,
+} from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, publicClient } = configureChains(
+    [mainnet, polygon, optimism, arbitrum, base, zora],
+    [
+        alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID || "no key" }),
+        publicProvider()
+    ]
+);
+
+const { connectors } = getDefaultWallets({
+    appName: 'Youtube',
+    projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT || "no key",
+    chains
+});
+
+const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors,
+    publicClient
+})
+
+export default function Wagmi({ children }: { children: React.ReactNode }) {
+    return (
+        <WagmiConfig config={wagmiConfig}>
+            <RainbowKitProvider chains={chains}>
+                {children}
+            </RainbowKitProvider>
+        </WagmiConfig>
+    )
+}
+
+
+
